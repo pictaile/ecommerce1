5e97d833bf9e7bd5a84e983149cfb812ef130787
kostya <kostya.ochotnik@gmail.com>
2019-05-30 21:40:42 +0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './main/users/users.component';
+import { PersonComponent } from './main/person/person.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'users/1', 
+		pathMatch: 'full'
+	},
+	{
+		path: 'users/:page',
+		component: UsersComponent
+	},
+	
+	{
+		path: 'person/:id',
+		component: PersonComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
